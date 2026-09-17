0c1aeff234abfb2519597065f6e1565792c99109
adewale85 <iseoluwadu85@gmail.com>
2026-09-17 09:42:13 +0100

    I connect my product to my dashboard product card to display available product in my inventory

--- src/lib/api/dashboard/getDashboardStats.ts
+++ src/lib/api/dashboard/getDashboardStats.ts
@@ -1,11 +1,31 @@
-import { DashboardStatsResponse } from "@/types/dashboard";
-import { apiClient } from "../axiosPrivate";
+// import { DashboardStatsResponse } from "@/types/dashboard";
+// import { apiClient } from "../axiosPrivate";
+
 
 
 /**
  * Fetch core dashboard overview figures (totals for products, value, low stock, quantities)
  */
-export async function getDashboardStats() {
-  const response = await apiClient.get<{ data: DashboardStatsResponse }>("/dashboard/stats");
-  return response.data.data;
+// export async function getDashboardStats() {
+//   const response = await apiClient.get<{ data: DashboardStatsResponse }>("/dashboard/stats");
+//   return response.data.data;
+// }
+
+import { supabase } from "@/supabase/client";
+
+
+export async function getDashboardStats () {
+  const {count, error} = await supabase
+  .from ("products")
+  .select ("*", { count: "exact", head: true });
+
+  if (error) {
+    throw new Error (
+      "Error fetching total products: " + error.message
+    );
+  }
+  
+  return{
+    total_products: count ?? 0,
+  };
 }
